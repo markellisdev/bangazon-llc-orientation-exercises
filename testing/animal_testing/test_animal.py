64ae01ff260189620e39e0884eb214318fa49b73
Mark Ellis <markellisdev@gmail.com>
2017-01-11 06:29:21 -0600

    animal unittesting complete

--- testing/animal_testing/test_animal.py
+++ testing/animal_testing/test_animal.py
@@ -8,6 +8,7 @@ class TestAnimals(unittest.TestCase):
         print('Set up class')
         self.animal = Animal()
         self.dog = Dog("Spot")
+        self.dog.set_legs(4)
 
     def test_AnimalHasName(self):
         self.animal.name = "Fred"
@@ -16,6 +17,21 @@ class TestAnimals(unittest.TestCase):
     def test_SpeciesIsCorrect(self):
         self.animal.set_species("Amur Leopard")
         self.assertEqual(self.animal.species, "Amur Leopard")
+
+    def test_AnimalIsInstanceOfAnimal(self):
+        self.assertIsInstance(self.animal, Animal)
+
+    def test_DogIsInstanceOfDog(self):
+        self.assertIsInstance(self.dog, Dog)
+
+    def test_DogWalkSpeedIsCorrect(self):
+        self.dog.walk()
+        self.assertEqual(self.dog.speed, 0.8)
+
+    def test_AnimalWalkSpeedIsCorrect(self):
+        self.animal.set_legs(2)
+        self.animal.walk()
+        self.assertEqual(self.animal.speed, 0.2)
 
 if __name__ == '__main__':
     unittest.main()
